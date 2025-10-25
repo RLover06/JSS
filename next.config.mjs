be14473cbdd045c1ca0f5d3b7174697f700c58a0
RLover06 <regyescoamver@gmail.com>
2025-10-25 16:00:17 -0500

    Primer commit: todo el proyecto

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    outputFileTracingRoot: import.meta.dirname, // Define la raíz correcta
+    reactStrictMode: true, // Recomendado para detectar errores en desarrollo
+    swcMinify: true,       // Usa el compilador SWC para minificar (más rápido)
+  };
+  
+  export default nextConfig;
+  
